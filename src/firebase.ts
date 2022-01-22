020e3e85d4603842120e55a241bcf412ae11ce3c
iCalculated <fufa0001@gmail.com>
2022-01-22 16:13:14 -0600

    exports

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const analytics = getAnalytics(app);
+export const auth = getAuth();
