32bc22076fb6e4db4e9b7ab351018b85efe66fbd
weineb2 <benjamin.r.weiner@medtronic.com>
2022-01-22 19:44:47 -0600

    Basic frontend setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Navigation from "./components/navigation";
 
 function App() {
   return (
     <div className="App">
-      <Routes></Routes>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/upload.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Upload = () => {
+  return (
+    <div>
+      <h1>Upload a file</h1>
+    </div>
+  );
+};
+
+export default Upload;
