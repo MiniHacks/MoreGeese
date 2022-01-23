23e73c0d809557ca6ed0d7b19d6285a50f6ed2f3
weineb2 <benjamin.r.weiner@medtronic.com>
2022-01-23 00:06:18 -0600

    Finishes up download code

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { ref, getDownloadURL } from "firebase/storage";
 import { app, storage } from "../firebase";
+const downloadjs = require("downloadjs");
 
 const Download = ({
   onPageChange,
@@ -17,13 +18,7 @@ const Download = ({
   const handleImageDownload = async () => {
     const pathReference = ref(storage, `unprocessed/${fileId}.png`);
     const url = await getDownloadURL(pathReference);
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = "blob";
-    xhr.onload = (event) => {
-      const blob = xhr.response;
-    };
-    xhr.open("GET", url);
-    xhr.send();
+    downloadjs(url);
   };
 
   return (
