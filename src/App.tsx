1f7afd5d8ec0847579caca75db3e01253483e14e
Minnerva Zou <zou00086@umn.edu>
2022-01-23 02:44:06 -0600

    Add broken  fade-in

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Navigation from "./components/navigation";
@@ -9,15 +9,28 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // import "./App.css";
 import "./styles/page.css";
+import "./styles/fade.css"
 
 function App() {
+  let location = useLocation();
   return (
     <div className="App">
       <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+
+      <TransitionGroup component={null}>
+        <CSSTransition
+          timeout={1000}
+          classNames="fade"
+          key={location.key}
+          appear
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
