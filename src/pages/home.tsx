f8ab2aaf3c35e8a72d9685ce7ba47b713adf9bec
theraggedflygon <benweiner27@gmail.com>
2022-01-23 13:43:03 -0600

    Adds dynamic image extensions

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require("uuid");
 const Home = () => {
   const [page, setPage] = useState("get-started");
   const [fileId, setId] = useState("");
+  const [ext, setExt] = useState("");
 
   useEffect(() => {
     setId(uuidv4());
@@ -17,16 +18,32 @@ const Home = () => {
   const handlePageChange = (pageName: string) => {
     setPage(pageName);
   };
-  
+
+  const handleExtChange = (extension: string) => {
+    setExt(extension);
+  };
+
   switch (page) {
     case "get-started":
       return <Start onPageChange={handlePageChange} />;
     case "upload":
-      return <Upload onPageChange={handlePageChange} fileId={fileId} />;
+      return (
+        <Upload
+          onPageChange={handlePageChange}
+          onExtChange={handleExtChange}
+          fileId={fileId}
+        />
+      );
     case "loading":
       return <Loading onPageChange={handlePageChange} />;
     case "select-areas":
-      return <SelectAreas onPageChange={handlePageChange} fileId={fileId} />;
+      return (
+        <SelectAreas
+          onPageChange={handlePageChange}
+          fileId={fileId}
+          extension={ext}
+        />
+      );
     case "download":
       return <Download onPageChange={handlePageChange} fileId={fileId} />;
     default:
