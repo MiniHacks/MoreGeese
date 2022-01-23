d0e1d71bd91e1d1a7a7e800f7cce2e3a87fe1f2d
Minnerva Zou <zou00086@umn.edu>
2022-01-23 08:47:06 -0600

    Add iffy about styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,32 +1,35 @@
 import React from "react";
+import '../styles/about.css';
 
 const About = () => {
   return (
     <div className="page about">
-      <h1>What is this?</h1>
-      <p>
-        A tool to help protect the privacy of anyone that uses an image sharing
-        website. Upload an image, pick the parts that you want to blur out, and
-        in just a few seconds, get a new copy of the image ready to post to the
-        web.
-      </p>
-      <h1>How is my image processed?</h1>
-      <p>
-        We use a machine learning computer vision algorithm to detect text in
-        your images that you might want to blur out in order to keep your
-        location private. You then select which of the text you want to blur and
-        we produce a new copy of your image with the desired regions blurred
-        out.
-      </p>
-      <h1>What do you do with my images?</h1>
-      <p>
-        After you upload an image, we only process it and remove any text. After
-        you download the new copy, we delete both the original and edited
-        versions in order to keep your privacy safe.
-      </p>
-      <h1>Who made this?</h1>
-      <p>People who have mixed feelings about geese</p>
-      <img src="./goose.png" alt="goose" />
+      <div>
+        <h2>What is this?</h2>
+        <p>
+          A tool to help protect the privacy of anyone that uses an image sharing
+          website. Upload an image, pick the parts that you want to blur out, and
+          in just a few seconds, get a new copy of the image ready to post to the
+          web.
+        </p>
+        <h1>How is my image processed?</h1>
+        <p>
+          We use a machine learning computer vision algorithm to detect text in
+          your images that you might want to blur out in order to keep your
+          location private. You then select which of the text you want to blur and
+          we produce a new copy of your image with the desired regions blurred
+          out.
+        </p>
+        <h1>What do you do with my images?</h1>
+        <p>
+          After you upload an image, we only process it and remove any text. After
+          you download the new copy, we delete both the original and edited
+          versions in order to keep your privacy safe.
+        </p>
+        <h1>Who made this?</h1>
+        <p>People who have mixed feelings about geese</p>
+        <img src="./goose.png" alt="goose" />
+      </div>
     </div>
   );
 };
