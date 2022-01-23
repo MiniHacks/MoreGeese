8022edd1bc2ea2eee643f083d7462d210031cb70
iCalculated <fufa0001@gmail.com>
2022-01-23 08:33:09 -0600

    segmentation model

--- flask/main.py
+++ flask/main.py
@@ -11,9 +11,19 @@ app = Flask(__name__)
 !pip3 install pixellib
 """
 
+import pixellib
+from pixellib.semantic import semantic_segmentation
+
+model = "deeplabv3_xception65_ade20k.h5"
+segment_image = semantic_segmentation()
+segment_image = load_ade20k_model(model)
+
 @app.route("/")
 def index():
     return "hello world!"
+
+@app.route("/mask")
+def mask():
 
 if __name__ == "__main__":
     app.run(debug=True)
