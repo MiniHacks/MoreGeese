83a163c3bd82bccef91bf772a3b2a5dc94b2cdc6
iCalculated <fufa0001@gmail.com>
2022-01-23 08:30:48 -0600

    dependenciesa

--- flask/main.py
+++ flask/main.py
@@ -3,6 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+"""
+!pip3 install tensorflow
+!pip3 install opencv-python
+!pip3 install scikit-image
+!pip3 install pillow
+!pip3 install pixellib
+"""
 
 @app.route("/")
 def index():
