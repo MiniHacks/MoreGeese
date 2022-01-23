a1f79228ffed32237145ca8352dda0742864d22d
iCalculated <fufa0001@gmail.com>
2022-01-23 08:44:59 -0600

    masking route

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,8 @@ app = Flask(__name__)
 !pip3 install pixellib
 """
 
+import numpy as np
+import cv2
 import pixellib
 from pixellib.semantic import semantic_segmentation
 
@@ -22,8 +24,37 @@ segment_image = load_ade20k_model(model)
 def index():
     return "hello world!"
 
-@app.route("/mask")
+@app.route("/mask", methods=['POST'])
 def mask():
+    f = request.files['file']
+    segments, img = segment_image(f, output_image="mask_"+f)
+
+    ids = segments["class_ids"]
+    names = segments["class_names"]
+    mask = segments["masks"]
+
+    # g value for signs is 5
+    mask = img[..., 1] != 5
+    masked = img.copy()
+    masked[mask] = 0
+
+    gray_image = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_image, 50, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    notable_contours = filter(lambda c: len(c) > 10, contours)
+    zipped_contours = []
+    for contour in notable_contours:
+        c = np.squeeze(contour, axis=1)
+        zipped_points = []
+        for (x, y) in c:
+            zipped_points.append(x)
+            zipped_points.append(y)
+        zipped_contours.append(zipped_points)
+    return zipped_contours
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
